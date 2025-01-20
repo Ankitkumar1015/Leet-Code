c240023db807d7495b628c0db4c2e4d2e92bba1c
Ankitkumar1015 <166295635+Ankitkumar1015@users.noreply.github.com>
2025-01-21 01:15:20 +0530

    Time: 36 ms (70.04%), Space: 17.9 MB (10.53%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.py
@@ -0,0 +1,22 @@
+class Solution:
+    def reverse(self, x: int) -> int:
+        INT_MAX = 2**31 - 1  # Maximum 32-bit signed integer
+        INT_MIN = -2**31     # Minimum 32-bit signed integer
+
+        result = 0
+        sign = -1 if x < 0 else 1
+        x = abs(x)
+
+        while x != 0:
+            digit = x % 10  # Extract the last digit
+            x //= 10        # Remove the last digit
+
+        # Check for overflow
+            if result > (INT_MAX - digit) // 10:
+                return 0
+
+            result = result * 10 + digit
+
+        return sign * result
+
+        
