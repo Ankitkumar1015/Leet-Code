240313d605c021671c300fdc8ebdd9e58bfa2cfe
Ankitkumar1015 <166295635+Ankitkumar1015@users.noreply.github.com>
2025-09-06 12:59:40 +0530

    Time: 4 ms (77.13%), Space: 44.7 MB (34.93%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) return false;
+
+        char[] srr = s.toCharArray();
+        char[] trr = t.toCharArray();
+        Arrays.sort(srr);
+        Arrays.sort(trr);
+
+        return Arrays.equals(srr, trr);
+    }
+}
